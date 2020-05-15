df936ca8e54e3974476c4e0a8dd7c2142186ad73
magicbing <magicbingl@foxmail.com>
2020-05-15 16:26:33 +0800

    add stack

---
+++ src/stack/readme.md
@@ -0,0 +1,3 @@
+# stack 栈
+栈是一个先入后出的数据结构。
+Stack is a data structure which elements are first in and last out(FILO).
---
+++ src/stack/stack.js
@@ -0,0 +1,23 @@
+class Stack {
+  constructor() {
+    this.stack = []
+  }
+
+  add(item) {
+    this.stack.push(item)
+  }
+
+  delete() {
+    this.stack.pop()
+  }
+
+  last() {
+    const length = this.stack.length
+    if (length < 1) {
+      console.log('this stack has no element')
+      return
+    }
+    const result = this.stack[length - 1]
+    return result
+  }
+}
