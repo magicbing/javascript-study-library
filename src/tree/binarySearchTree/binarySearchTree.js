d4d594b28288728fe26c38797f70af22871d0d5b
magicbing <magicbingl@foxmail.com>
2020-05-20 16:15:34 +0800

    add bst functions

--- src/tree/binarySearchTree/binarySearchTree.js
+++ src/tree/binarySearchTree/binarySearchTree.js
@@ -1,6 +1,8 @@
 class TreeNode {
-  constructor(val) {
+  constructor(key, val, N) {
+    this.key = key
     this.val = val
+    this.N = N
     this.left = null
     this.right = null
   }
@@ -8,79 +10,144 @@ class TreeNode {
 
 class BinarySearchTree {
   constructor() {
-    this.tree = null
-    this.size = 0
+    this.root = null
   }
-
-  add(val) {
-    const newNode = new TreeNode(val)
-    const curNode = this.tree
-
-    const core = function(val, tree) {
-      if (tree === null || val === tree.val) {
-        return tree
-      }
-      if (val < tree.val) {
-        if (tree.left === null) {
-          return tree
-        } else {
-          return core(val, tree.left)
-        }
-      } else {
-        if (tree.right === null) {
-          return tree.right
-        } else {
-          return core(val, tree.right)
-        }
-      }
-    }
-
-    if (curNode === null) {
-      this.tree = newNode
+  
+  size(x = this.root) {
+    if (x === null) {
+      return 0
     } else {
-      console.log('add else')
-      let temp = core(val, curNode)
-      console.log(temp)
-      temp.left = newNode
+      return x.N
     }
   }
 
-  get(val) {
-    console.log('get')
-    const core = function(val, tree) {
-      if (tree === null || val === tree.val) {
-        return tree
+  get(key) {
+    const core = function (x = this.root, key) {
+      if (x === null) {
+        return null
       }
-      if (val < tree.val) {
-        if (tree.left === null) {
-          return tree.left
-        } else {
-          return core(val, tree.left)
-        }
+  
+      if (key < x.key) {
+        return this.get(x.left, key)
+      } else if (key > x.key) {
+        return this.get(x.right, key)
       } else {
-        if (tree.right === null) {
-          return tree.right
-        } else {
-          return core(val, tree.right)
-        }
+        return x.val
       }
     }
 
-    const result = core(val, this.tree)
-    console.log(result === this.tree.left, result)
-    return result
+    return core(this.root, key)
   }
 
-  size() {
-    return this.size
+  put(key, val) {
+    const size = this.size
+    const core = function (x, key, val) {
+      if (x === null) {
+        return new TreeNode(key, val, 1)
+      }
+  
+      if (key < x.key) {
+        x.left = core(x.left, key, val)
+      } else if (key > x.key) {
+        x.right = core(x.right, key, val)
+      } else {
+        x.val = val
+      }
+      x.N = size(x.left) + size(x.right) + 1
+  
+      return x
+    }
+
+    this.root = core(this.root, key ,val)
   }
 
-  delete(val) {
+  max() {
+    const core = function(x) {
+      if (x.right === null) {
+        return x
+      }
+      return core(x.right)
+    }
 
+    return core(this.root).key
   }
+
+  min() {
+    const core = function(x) {
+      if (x.left === null) {
+        return x
+      }
+      return core(x.left)
+    }
+
+    return core(this.root).key
+  }
+
+  floor(key) {
+    const core = function(x, key) {
+      if (x === null) {
+        return null
+      }
+      if (key === x.key) {
+        return x
+      } else if (key < x.key) {
+        return core(x.left, key)
+      }
+      const t = core(x.right, key)
+      if (t !== null) {
+        return t
+      } else {
+        return x
+      }
+    }
+
+    const x = core(this.root, key)
+    if (x === null) {
+      return null
+    }
+
+    return x.key
+  }
+
+  ceiling(key) {
+    const core = function(x, key) {
+      if (x === null) {
+        return null
+      }
+      if (key === x.key) {
+        return x
+      } else if (key > x.key) {
+        return core(x.right, key)
+      }
+      const t = core(x.left, key)
+      if (t !== null) {
+        return t
+      } else {
+        return x
+      }
+    }
+
+    const x = core(this.root, key)
+    if (x === null) {
+      return null
+    }
+    return x.key
+  }
+
+  select() {}
+
+  rank() {}
+
+  delete() {}
+
+  deleteMin() {}
+
+  deleteMax() {}
+
+  keys() {}
 }
 
 q = new BinarySearchTree()
-q.add(2)
-q.add(1)
-q.add(3)
+q.put(3, 3)
+q.put(1, 1)
+q.put(5, 5)
